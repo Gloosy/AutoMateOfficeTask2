281d0440a5d4edfaefcef48090c9d35e8c10add3
Sreynoch <sreynoch.siv@student.cadt.edu.kh>
2024-09-21 22:43:49 +0700

    Nochh

--- interactive_chart_project/celerybeat_schedule.py
+++
@@ -1,8 +0,0 @@
-from celery.schedules import crontab
-
-app.conf.beat_schedule = {
-    'generate-chart-every-week': {
-        'task':git  'tasks.generate_weekly_chart',
-        'schedule': crontab(0, 0, day_of_week='mon'),  # Every Monday at midnight
-    },
-}
